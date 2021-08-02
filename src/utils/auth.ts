8d36dd8201cca274dc79359395abb0d90b354cd2
KAthousand <KAthousand@gmail.com>
2021-08-02 10:03:42 -0400

    added basic util with jsonwebtoken and bcrypt

---
+++ src/utils/auth.ts
@@ -0,0 +1,44 @@
+//imports
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+//import env config
+import { env } from '../config/env'
+
+//import types
+import { Request, Response, NextFunction } from 'express';
+
+//load env to get access keys and refresh keys
+const accessTokenKey = env.get('ACCESS_TOKEN_KEY')
+
+
+//hash user's passwords (when creating user)
+export const hashPassword = (pass: string) => {
+  const hashedPassword = bcrypt.hash(pass, 10);
+  return hashedPassword;
+}
+
+//generate a json web token (when creating user)
+export const generateToken = (user: object) => {
+  const accessToken = jwt.sign(user, accessTokenKey as jwt.Secret,
+  { expiresIn: '24h' })
+  return accessToken
+}
+
+//compare bcrypted pass with entered pass
+export const comparePass = (pass: string, userPass: string) => {
+  return bcrypt.compare(pass, userPass)
+}
+
+//authenticate token in header
+export const authenticateToken = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.status(401).send('Invalid Token')
+
+  jwt.verify(token, accessTokenKey as string, (err, user) => {
+    if (err) return res.status(403).send(err.message)
+    req.body.user = user
+    next()
+  })
+}
